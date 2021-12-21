4b6cba3f3efb35e2a6b1eb46939f742697074a39
عمر ايمن السعيد ابو والى شريف stu <321213367@sha.edu.eg>
2021-12-21 15:06:52 +0200

    xz

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.performanceRAM)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.performanceCPU)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -52,10 +53,11 @@
             // progressBar1
             // 
             this.progressBar1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.progressBar1.Location = new System.Drawing.Point(238, 135);
+            this.progressBar1.Location = new System.Drawing.Point(238, 109);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(485, 31);
             this.progressBar1.TabIndex = 0;
+            this.progressBar1.Visible = false;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // progressBar2
@@ -65,16 +67,18 @@
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(485, 30);
             this.progressBar2.TabIndex = 1;
+            this.progressBar2.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(189, 135);
+            this.label1.Location = new System.Drawing.Point(189, 109);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 23);
             this.label1.TabIndex = 2;
             this.label1.Text = "CPU";
+            this.label1.Visible = false;
             // 
             // label2
             // 
@@ -85,16 +89,18 @@
             this.label2.Size = new System.Drawing.Size(48, 23);
             this.label2.TabIndex = 2;
             this.label2.Text = "RAM";
+            this.label2.Visible = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(729, 135);
+            this.label3.Location = new System.Drawing.Point(729, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "00%";
+            this.label3.Visible = false;
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
@@ -106,6 +112,7 @@
             this.label4.Size = new System.Drawing.Size(49, 23);
             this.label4.TabIndex = 2;
             this.label4.Text = "00%";
+            this.label4.Visible = false;
             // 
             // performanceRAM
             // 
@@ -132,6 +139,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(154, 116);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // pictureBox2
             // 
@@ -143,6 +151,7 @@
             this.pictureBox2.Size = new System.Drawing.Size(154, 105);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // button1
@@ -152,9 +161,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(363, 3);
+            this.button1.Location = new System.Drawing.Point(172, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 33);
+            this.button1.Size = new System.Drawing.Size(477, 33);
             this.button1.TabIndex = 6;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = false;
@@ -166,13 +175,27 @@
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(363, 42);
+            this.button2.Location = new System.Drawing.Point(172, 42);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 39);
+            this.button2.Size = new System.Drawing.Size(477, 39);
             this.button2.TabIndex = 7;
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(655, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 78);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -182,6 +205,7 @@
             this.BackColor = System.Drawing.SystemColors.MenuBar;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 319);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
@@ -223,6 +247,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -55,18 +55,48 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            progressBar1.Visible = true;
+            progressBar2.Visible = true;
+            label1  .Visible = true;
+            label2 .Visible = true; 
+            label3 .Visible = true;
+            label4 .Visible = true; 
+            pictureBox1 .Visible = true;
+            pictureBox2 .Visible = true;
             timer1.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            timer1.Stop();
-            
+
+            label3.Text = "00%";
+            label4.Text = "00%";
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
             label3.Text = "00 %";
             label4.Text = "00 %";
             progressBar1.Value = 0;
             progressBar2.Value = 0;
+            progressBar1.Visible = false;
+            progressBar2.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1 .Stop();
             
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
         }
     }
 }
